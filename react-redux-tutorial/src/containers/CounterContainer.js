efaa766cd6acd0cc216bf534ae7c6d46bdd3e11e
leeks123 <kensei719@naver.com>
2020-09-06 21:10:05 +0900

    리다기

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,71 @@
+import React, { useCallback } from "react";
+import { bindActionCreators } from "redux";
+import { connect, useSelector, useDispatch } from "react-redux";
+import Counter from "../components/Counter";
+import { increase, decrease } from "../modules/counter";
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+export default CounterContainer;
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// export default connect(
+//     (state) => ({
+//       number: state.counter.number,
+//     }),
+//     {
+//       increase,
+//       decrease,
+//     }
+//   )(CounterContainer);
+
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+//------------------------------------------------
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) => ({
+//     increase: () => dispatch(increase()),
+//     decrease: () => dispatch(decrease()),
+//   })
+// )(CounterContainer);
+
+//------------------------------------------------
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) =>
+//     bindActionCreators(
+//       {
+//         increase,
+//         decrease,
+//       },
+//       dispatch
+//     )
+// )(CounterContainer);
---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from "react";
+import TodoListItem from "./TodoListItem";
+import { List } from "react-virtualized";
+import "./TodoList.scss";
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512} // 전체 크기
+      height={513} // 전체 높이
+      rowCount={todos.length} // 항목 개수
+      rowHeight={57} // 항목 높이
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: "none" }}
+    />
+  );
+};
+
+export default React.memo(TodoList);
